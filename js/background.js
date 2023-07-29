3771540079147dc11faf0b8723d644594f01c2d1
Anatoliy <123824534+YaguraWizz@users.noreply.github.com>
2023-07-30 01:57:13 +0500

    implemented data forwarding and writing them as text

--- js/background.js
+++ js/background.js
@@ -1,57 +1,91 @@
-chrome.tabs.query({}, function (tabs) {
-  var pages = []; // Массив для хранения информации о страницах
-  var groups = {}; // Объект для хранения информации о группах
+var firstMeaning;
 
-  // Проход по каждой вкладке и добавление информации в массив страниц или объект групп
-  tabs.forEach(function (tab) {
-    if (tab.groupId === -1) {
-      // Страница не принадлежит к какой-либо группе
-      var page = {
-        id: tab.id,
-        url: tab.url,
-        title: tab.title,
-      };
-      pages.push(page);
-    } else {
-      // Страница принадлежит к группе
-      var groupId = tab.groupId;
-      if (!groups[groupId]) {
-        groups[groupId] = [];
-      }
-      var page = {
-        id: tab.id,
-        url: tab.url,
-        title: tab.title,
-      };
-      groups[groupId].push(page);
-    }
-  });
-  // JSON-объект, содержащий информацию о страницах и группах
-  var jsonData = {
-    pages: pages,
-    groups: groups,
-  };
-
-  console.log(jsonData); // Ваши данные в формате JSON
-  // Преобразовываем JSON-объект в строку JSON
-  var jsonString = JSON.stringify(jsonData);
-  // Сохраняем строку JSON в хранилище
-  chrome.storage.local.set({ myKey: jsonString }, function () {
-    console.log("JSON-объект сохранен в хранилище");
+// Вызываем функцию data_collection() при запуске браузера
+chrome.runtime.onStartup.addListener(function () {
+  data_collection(function (data) {
+    firstMeaning = data;
   });
 });
 
-/*
-console.log("hello my extension");
+chrome.tabs.onCreated.addListener(function (tab) {
+  // Вызывайте функцию обновления здесь для обработки открытия новой вкладки
+  updateData();
+});
 
-data_collection();
+chrome.tabs.onRemoved.addListener(function (tabId, removeInfo) {
+  // Вызывайте функцию обновления здесь для обработки закрытия вкладки
+  updateData();
+});
 
-function data_collection() {
-  chrome.tabs.query({}, function (tabs) {
-    tabs.forEach(function (tab) {
-      console.log(tab.title);
-    });
+// Функция обновления данных
+function updateData() {
+  data_collection(function (data) {
+    var secondMeaning = data;
+
+    // Сравниваем объекты JSON по равенству
+    if (JSON.stringify(firstMeaning) !== JSON.stringify(secondMeaning)) {
+      // Вызываем функцию messagesDataCollection()
+      messagesDataCollection(secondMeaning);
+    } else {
+      console.log("изменений нет");
+    }
   });
 }
 
- */
+// Функция отправки json объекта
+function messagesDataCollection(jsonData) {
+  var jsonString = JSON.stringify(jsonData);
+  // Сохраняем строку JSON в хранилище
+  saveDataToStorage(jsonString, function () {
+    console.log("JSON-объект сохранен в хранилище");
+  });
+}
+
+// Функция сохранения данных в хранилище
+function saveDataToStorage(jsonData, callback) {
+  chrome.storage.local.set({ myKey: jsonData }, function () {
+    console.log("JSON-объект сохранен в хранилище");
+    // Вызываем колбэк-функцию после сохранения данных
+    callback();
+  });
+}
+
+function data_collection(callback) {
+  chrome.tabs.query({}, function (tabs) {
+    var pages = []; // Массив для хранения информации о страницах
+    var groups = {}; // Объект для хранения информации о группах
+    // Проход по каждой вкладке и добавление информации в массив страниц или объект групп
+    tabs.forEach(function (tab) {
+      if (tab.groupId === -1) {
+        // Страница не принадлежит к какой-либо группе
+        var page = {
+          id: tab.id,
+          url: tab.url,
+          title: tab.title,
+        };
+        pages.push(page);
+      } else {
+        // Страница принадлежит к группе
+        var groupId = tab.groupId;
+        if (!groups[groupId]) {
+          groups[groupId] = [];
+        }
+        var page = {
+          id: tab.id,
+          url: tab.url,
+          title: tab.title,
+        };
+        groups[groupId].push(page);
+      }
+    });
+    // JSON-объект, содержащий информацию о страницах и группах
+    var jsonData = {
+      pages: pages,
+      groups: groups,
+    };
+
+    console.log(jsonData); // Ваши данные в формате JSON
+    // Возврат значения через callback
+    callback(jsonData);
+  });
+}
--- js/popup.js
+++ js/popup.js
@@ -1,23 +1,45 @@
-// Регистрируем обработчик изменений
-chrome.storage.onChanged.addListener(storageChanged);
+function processSavedData() {
+  // Здесь вы можете получить сохраненные данные
+  chrome.storage.local.get("myKey", function (result) {
+    var storedJsonString = result.myKey;
+    console.log("Получены данные из хранилища:", storedJsonString);
+    // Продолжайте обработку данных здесь
 
-// Определяем функцию обработчика изменений хранилища
-function storageChanged(changes, areaName) {
-  if (areaName === "local") {
-    console.log("Произошло изменение в локальном хранилище:", changes);
-    chrome.storage.local.get("myKey", function (result) {
-      var storedJsonString = result.myKey;
+    var storedObject = JSON.parse(storedJsonString);
+    var listElement = document.getElementById("titleList");
+    listElement.innerHTML = "";
 
-      // Преобразовываем строку JSON обратно в объект
-      var storedObject = JSON.parse(storedJsonString);
-      var listElement = document.getElementById("titleList");
-      // Проходим по элементам данных и добавляем их в список
-      storedObject.items.forEach(function (item) {
+    // Проверяем наличие элементов pages и groups
+    if ("pages" in storedObject) {
+      storedObject.pages.forEach(function (page) {
         var listItem = document.createElement("li");
-        listItem.textContent = item;
+        listItem.textContent = page.title;
         listElement.appendChild(listItem);
       });
-      console.log(storedObject);
-    });
-  }
+    }
+
+    if ("groups" in storedObject) {
+      for (var groupId in storedObject.groups) {
+        var group = storedObject.groups[groupId];
+        group.forEach(function (page) {
+          var listItem = document.createElement("li");
+          listItem.textContent = page.title;
+          listElement.appendChild(listItem);
+        });
+      }
+    }
+    console.log(storedObject);
+  });
 }
+
+function buttonHandler() {
+  // Выполните необходимые действия при нажатии кнопки
+  console.log("Кнопка была нажата!");
+  processSavedData();
+}
+
+// Находим кнопку по идентификатору
+var button = document.getElementById("myButton");
+
+// Добавляем обработчик события при нажатии на кнопку
+button.addEventListener("click", buttonHandler);
